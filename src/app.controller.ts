c4239cbf43587ecc60586541481bae3d0b21cb4b
farik <faha.azizov@gmail.com>
2024-09-09 11:39:44 +0500

    Add new endpoints and swagger ui

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AppService } from './app.service';
 
-@Controller()
+@ApiTags('Notes')
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('create')
+  createHello() {
+    return this.appService.createHello();
   }
 }
